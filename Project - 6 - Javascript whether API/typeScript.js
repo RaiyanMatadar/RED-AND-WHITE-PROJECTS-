3b4c186468ae84a6ef01fdc692148e0716c051c7
RaiyanMatadar <raiyanmatadar001@gmail.com>
2026-01-13 00:05:58 +0530

    weather API with search by city & place name,  Added 7 days forcast as well in weather API

--- Project - 6 - Javascript whether API/typeScript.js
+++ Project - 6 - Javascript whether API/typeScript.js
@@ -14,13 +14,26 @@
 const input = document.getElementById("search");
 const button = document.querySelector("button")
 const outputWhether = document.getElementById("outputWhether");
+const forcast = document.getElementById("forcast");
 
 async function fetchingData(inputCity) {
     try {
         let responseCurrentWheather = await fetch(`http://api.weatherapi.com/v1/current.json?key=14cbafa8a4e343a7b6675447261201&q=${inputCity}&aqi=no`)
         let dataCurrentWheather = await responseCurrentWheather.json();
         displayData(dataCurrentWheather);
+        console.log(dataCurrentWheather);
+        
+        let lat = dataCurrentWheather.location.lat;
+        let lon = dataCurrentWheather.location.lon;
+
+        let APIForcast = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,relative_humidity_2m_max&forecast_days=7`
+        let responseForcastWheather = await fetch(APIForcast);
+        let dataForcastWheather = await responseForcastWheather.json();
+        
+        console.log(dataForcastWheather);
+        displayForcastData(dataForcastWheather);
     
+
     } catch (error) {
         console.log("couldnt get the whether data");
     }
@@ -46,5 +59,27 @@ function displayData(displayParam){
             <p>longtitute : ${lon}</p>
             <p> temprature : ${temp_c} °C </p>
         `
+    })
+}
+
+function displayForcastData(displayParam){
+    button.addEventListener("click",(event)=>{
+    
+        let diplsay = displayParam.daily;
+        let box = "";
+
+        for (let i = 0; i < 7; i++){
+            box += `
+            <div class="day-box">
+                <p><b>Day ${i+1}</b></p>
+                <p>Temp Max: ${diplsay.temperature_2m_max[i]}°C</p>
+                <p>Temp Min: ${diplsay.temperature_2m_min[i]}°C</p>
+                <p>Humidity: ${diplsay.relative_humidity_2m_max[i]}%</p>
+                <p>Rain: ${diplsay.precipitation_sum[i]} mm</p>
+            </div>
+            `;
+        }
+
+        forcast.innerHTML = box;
     })
 }
